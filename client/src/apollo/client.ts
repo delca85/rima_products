b735b636a2ef1d038c12a91127fbb3651c060fb2
Bianca Del Carretto <bianca.delcarretto@hudl.com>
2020-09-25 22:34:51 +0200

    rename server start script, restore local graphql uri for client and add client build to gitignore

--- client/src/apollo/client.ts
+++ client/src/apollo/client.ts
@@ -6,8 +6,7 @@ const buildApolloClient = () => {
   const cache = new InMemoryCache();
 
   return new ApolloClient({
-    // uri: graphql,
-    uri: "../server/functions/graphql",
+    uri: graphql,
     cache,
   });
 };
