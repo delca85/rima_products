65ab9b89ba3b0da372bc58a8927644050a1a7c15
Bianca Del Carretto <bianca.delcarretto@hudl.com>
2020-09-26 23:51:17 +0200

    remove unused code

--- client/src/apollo/client.ts
+++ client/src/apollo/client.ts
@@ -1,8 +1,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
-import { getEnvironmentConfiguration } from '../configuration/environmentConfiguration';
+// import { getEnvironmentConfiguration } from '../configuration/environmentConfiguration';
 
 const buildApolloClient = () => {
-  const { graphql } = getEnvironmentConfiguration();
+  // const { graphql } = getEnvironmentConfiguration();
   const cache = new InMemoryCache();
 
   return new ApolloClient({
