511a19d5bd271316561e69ff9c3e86a45306d198
Bianca Del Carretto <bianca.delcarretto@hudl.com>
2020-09-26 23:32:55 +0200

    add ts ignore

--- server/src/macro-product/index.ts
+++ server/src/macro-product/index.ts
@@ -1,3 +1,4 @@
 export { default as MacroProductModel, MacroProductModelStatic } from './macroProduct.model';
+//@ts-ignore
 export { default as macroProductTypeDefs } from './macroProduct.typeDefs';
 export { default as macroProductResolvers } from './macroProduct.resolvers';
--- server/src/macro-product/macroProduct.typedefs.types.ts
+++
@@ -1,8 +0,0 @@
-import { MacroProduct as tMacroProduct, Query as tQuery } from "../types/graphql.generated";
-
-interface tMacroProductTypeDefs {
-    MacroProduct: tMacroProduct
-    Query: tQuery,
-}
-
-export default tMacroProductTypeDefs;
