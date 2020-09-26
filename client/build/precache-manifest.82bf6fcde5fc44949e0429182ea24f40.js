6b2f3a6f7a640add5c1617533541f4f18c0fd048
Bianca Del Carretto <bianca.delcarretto@hudl.com>
2020-09-27 00:26:17 +0200

    change build folder in netlify and remove from gitignore

---
+++ client/build/precache-manifest.82bf6fcde5fc44949e0429182ea24f40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14f280d44c2be526c60436612d6e6e91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74e6f18f82816cbf6206",
+    "url": "/static/js/2.03581669.chunk.js"
+  },
+  {
+    "revision": "da42135372d3365a7233548ad1382c4d",
+    "url": "/static/js/2.03581669.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30b13ee30488c71bca05",
+    "url": "/static/js/main.d2dae1ad.chunk.js"
+  },
+  {
+    "revision": "c6a022824e3f43190834",
+    "url": "/static/js/runtime-main.aacf818b.js"
+  }
+]);
