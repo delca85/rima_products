5f1de173b05701a0cf6eda8588371e0a72ed0d1b
Bianca Del Carretto <bianca.delcarretto@hudl.com>
2020-10-03 16:20:31 +0200

    add graphql server max depth

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,31 +1,31 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server';
 import { ApolloServer as ApolloServerLambda } from 'apollo-server-lambda';
+import depthLimit from 'graphql-depth-limit';
 import { partTypeDefs, partResolvers } from './part';
 import { macroProductTypeDefs, macroProductResolvers } from './macro-product';
 import { createContext } from './context';
-
-// const app = express();
 
 const typeDefs = [partTypeDefs, macroProductTypeDefs];
 const resolvers = [partResolvers, macroProductResolvers];
 const context = createContext();
 
-export const createLambdaServer = () => new ApolloServerLambda({
-  typeDefs,
-  resolvers,
-  introspection: true,
-  playground: true,
-  context
-})
+export const createLambdaServer = () =>
+  new ApolloServerLambda({
+    typeDefs,
+    resolvers,
+    introspection: true,
+    playground: true,
+    validationRules: [depthLimit(2)],
+    context,
+  });
 
-export const createLocalServer = () => new ApolloServer({
-  typeDefs,
-  resolvers,
-  introspection: true,
-  playground: true,
-  context
-})
-
-
-// server.applyMiddleware({ app });
+export const createLocalServer = () =>
+  new ApolloServer({
+    typeDefs,
+    resolvers,
+    introspection: true,
+    playground: true,
+    validationRules: [depthLimit(3)],
+    context,
+  });
