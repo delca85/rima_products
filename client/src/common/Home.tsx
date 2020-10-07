70fdce90cf82e8dfe867615f60f9b1a60add1e2d
Bianca Del Carretto <bianca.delcarretto@hudl.com>
2020-10-07 15:11:29 +0200

    add link in home page

--- client/src/common/Home.tsx
+++ client/src/common/Home.tsx
@@ -1,10 +1,16 @@
 import { default as React } from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { IRimaComponentProps } from '../types/component.types';
 
 const HomeRaw = ({ className }: IRimaComponentProps) => (
   <div className={className}>
-    <span>Under construction...</span>
+    <Link
+      to={{
+        pathname: '/macro_product/1',
+      }}>
+      Macro product with id: 1
+    </Link>
   </div>
 );
 
